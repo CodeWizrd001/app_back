effce8970f47c3005f3e0a10d3dcab6d3bc7f4c7
DragonPyli <codewizard001@gmail.com>
2020-03-19 20:38:55 +0530

    Minor Fixes

--- web/__main__.py
+++ web/__main__.py
@@ -1,10 +1,27 @@
 import asyncio
 import sanic
+import random
 from sanic import Sanic
 from sanic.response import json
 import sys
 
 app = Sanic(__name__)
+
+nameList = ['Olivia', 'Ruby', 'Emily', 'Grace', 'Jessica', 'Chloe', 
+'Sophie', 'Lily', 'Amelia', 'Evie', 'Mia', 'Ella', 'Charlotte', 
+'Lucy', 'Megan', 'Ellie', 'Isabelle', 'Isabella', 'Hannah', 'Katie', 
+'Ava', 'Holly', 'Summer', 'Millie', 'Daisy', 'Phoebe', 'Freya', 
+'Abigail', 'Poppy', 'Erin', 'Emma', 'Molly', 'Imogen', 'Amy', 'Jasmine', 
+'Isla', 'Scarlett', 'Leah', 'Sophia', 'Elizabeth', 'Eva', 'Brooke', 
+'Matilda', 'Caitlin', 'Keira', 'Alice', 'Lola', 'Lilly', 'Amber', 
+'Isabel', 'Lauren', 'Georgia', 'Gracie', 'Eleanor', 'Bethany', 
+'Madison', 'Amelie', 'Isobel', 'Paige', 'Lacey', 'Sienna', 'Libby', 
+'Maisie', 'Anna', 'Rebecca', 'Rosie', 'Tia', 'Layla', 'Maya', 'Niamh', 
+'Zara', 'Sarah', 'Lexi', 'Maddison', 'Alisha', 'Sofia', 'Skye', 
+'Nicole', 'Lexie', 'Faith', 'Martha', 'Harriet', 'Zoe', 'Eve', 'Julia', 
+'Aimee', 'Hollie', 'Lydia', 'Evelyn', 'Alexandra', 'Maria', 'Francesca', 
+'Tilly', 'Florence', 'Alicia', 'Abbie', 'Emilia', 'Courtney', 'Maryam', 'Esme']
+
 
 class Stu :
     def __init__(self,name=None,age=0) :
@@ -20,7 +37,7 @@ def handleResp(request) :
     a = {}
     b = []
     for i in range(100) :
-        A = Stu("None",i)
+        A = Stu(random.choice(nameList),random.randint(1,random.choice([2,50,100])))
         a[i] = A._dict()
         b.append(A._dict()) 
     return json({'data':b})
